cbe2e831d7700efa9fa7f308ebb0fe06d5613908
oviozz <42685801+oviozz@users.noreply.github.com>
2023-09-13 00:53:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,6 @@ def hello_world():
 def hello_apple():
     return 'Hello Apple!'
 
-@app.route('/banana')
-def hello_banana():
-    return 'Hello Banana'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0",port= 5000)
